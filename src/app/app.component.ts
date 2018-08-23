36d1fb5d597602378f60dd0e29bede7e3debf597
woodRock <woodjess3@ecs.ac.nz>
2018-08-23 16:35:10 +1200

    Finished up to imputation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'WoodRocks Emporium of Shite';
+  title = 'WoodRock';
 }
--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -7,5 +7,7 @@ export const PROJECTS: ProjectItem[] = [
 
   { title:"SCARA", description: 'Selective Compliance Assembly Robot Arm.', image:'assets/images/elf.jpg', button:null },
 
-  { title:"Barbie2016", description: 'Autonomous Vehicle Challenge.', image:'assets/images/barbie.jpg', button:null }
+  { title:"Barbie2016", description: 'Autonomous Vehicle Challenge.', image:'assets/images/barbie.jpg', button:null },
+
+  { title:"Portfolio Website", description: 'This is my portfolio website written using angular java script.', image:'assets/images/bg_2880.jpg', button:null }
 ];
