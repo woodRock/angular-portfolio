6d85b10fc7b9e026a3acd19c0e5b5b5db91c43ec
woodRock <woodjess3@ecs.ac.nz>
2018-08-25 02:19:41 +1200

    Got the projects page looking fantastic

--- src/app/mock-features.ts
+++ src/app/mock-features.ts
@@ -3,18 +3,18 @@ import { Feature } from './feature';
 export const FEATURES: Feature[] = [
     {title: "Hi, I'm Jesse", 
     description: "Welcome to my portfolio of work", backgroundImage: "assets/images/bg_2880.jpg",
-    linkImage: "git.png", 
-    link:""}, 
+    linkImage: "assets/images/git.png", 
+    link:"Github"}, 
 
     {title: "The-Finding-of-I-Zack", 
     description: "This is a sub-aquatic immersive 2D dungeon crawler that will sink your expectations to new depths.", 
     backgroundImage: "assets/images/finding.jpg",
-    linkImage: "", 
-    link:""},
+    linkImage: "assets/images/git.png", 
+    link:"View Source"},
     
     {title: "Learn more about me...", 
     description: "My name is Jesse Wood. I am student at Victora University studying towards a degree in Software Engineering. I am familiar with, but by know means an expert...", 
     backgroundImage: "assets/images/tank.jpg",
     linkImage: "", 
-    link:""},
+    link:"About me"},
 ];
